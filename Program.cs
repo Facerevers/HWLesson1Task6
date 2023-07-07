baad4538d7b8779190113867b66be02d52bf03b2
crazle <crazle@mail.ru>
2023-07-07 17:55:13 +0300

    Создали программу, определяющую, чётное/нечётное число

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0) Console.WriteLine("Это число чётное");
+else Console.WriteLine("Это число нечётное");
